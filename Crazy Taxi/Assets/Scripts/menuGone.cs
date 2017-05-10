fddd42ca27b5c8e1b5f95773cc14be61c00acfd0
kapatka2 <kapatka2@illinois.edu>
2017-05-10 12:12:58 -0500

    added menu

---
+++ Crazy Taxi/Assets/Scripts/menuGone.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menuGone : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float forward_movement = Input.GetKey ("w") ? 1.0f : 0.0f;
+		float backward_movement = Input.GetKey ("s") ? 1.0f : 0.0f;
+
+		/*
+        float forward_movement = Input.GetAxis("Oculus_GearVR_RIndexTrigger");
+        float backward_movement = Input.GetAxis("Oculus_GearVR_LIndexTrigger");
+        */
+
+		if (forward_movement > 0 || backward_movement > 0) {
+			gameObject.SetActive (false);
+		}
+	}
+}
---
+++ Crazy Taxi/Assets/Scripts/resetScene.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class resetScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Return) || Input.GetKey ("enter")) {
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		}
+	}
+}
