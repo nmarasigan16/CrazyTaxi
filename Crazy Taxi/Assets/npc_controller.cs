109ff0962d606fcc143c9fe0a84a98361eb2a480
nmarasigan16 <egarsenal16@gmail.com>
2017-05-09 21:25:41 -0500

    added npc scripts

---
+++ Crazy Taxi/Assets/npc_controller.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class npc_controller : MonoBehaviour {
+    Animator anim;
+    bool hailing;
+    private string[] animClipNameGroup;
+    Vector3 destination;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+
+        animClipNameGroup = new string[] {
+            "Teat_01",
+            "Basic_Walk_01",
+            "Basic_Walk_02",
+            "Etc_Walk_Zombi_01"
+
+        };
+
+        anim = gameObject.GetComponent<Animator>();
+        hailing= false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!hailing)
+        {
+            move();
+        }
+
+	}
+
+    void chooseAnimation()
+    {
+
+    }
+
+    void move()
+    {
+        transform.Translate(Vector3.forward * 5 * Time.deltaTime);
+    }
+
+    void hailTaxi()
+    {
+        anim.Play(animClipNameGroup[0]);
+        Vector3 up = new Vector3(0, 2.5f, 0);
+        transform.position = transform.position + up;
+        //generate destination
+        //pass to some trigger?
+    }
+    
+    void stopHailing()
+    {
+
+    }
+
+    void turn(string direction)
+    {
+        Quaternion rot = Quaternion.identity;
+        if (direction == "left")
+        {
+            rot.eulerAngles = new Vector3(0, -90, 0);
+        }
+        if (direction == "right")
+        {
+            rot.eulerAngles = new Vector3(0, 90, 0);
+        }
+        if (direction == "around")
+        {
+            rot.eulerAngles = new Vector3(0, 0, 180);
+        }
+        transform.rotation *= rot;
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "street_corner")
+        {
+            float should_turn = Mathf.Round(Random.value * 100 + 1);
+            if(should_turn > 70)
+            {
+                StartCoroutine(wait_turn(1, "left"));
+            }
+            else if(should_turn > 40)
+            {
+                StartCoroutine(wait_turn(1f, "right"));
+            }
+
+        }
+    }
+
+    IEnumerator wait_turn(float time, string direction)
+    {
+        yield return new WaitForSeconds(time);
+        turn(direction);
+    }
+}
---
+++ Crazy Taxi/Assets/scripts/NPCCarY.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCCarY : MonoBehaviour {
+
+    private Rigidbody car;
+    private bool x = true;
+    private int turned_collisions;
+    private int speed = 20;
+    private int collisions;
+    Vector3 forward = new Vector3(1, 0, 0);
+
+    // Use this for initialization
+    void Awake()
+    {
+        car = GetComponent<Rigidbody>();
+        collisions = 0;
+        turned_collisions = 0;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        move();
+    }
+
+    void turn(string direction)
+    {
+        Quaternion rot = Quaternion.identity;
+        if (direction == "left")
+        {
+            rot.eulerAngles = new Vector3(0, -90, 0);
+        }
+        if (direction == "right")
+        {
+            rot.eulerAngles = new Vector3(0, 90, 0);
+        }
+        if (direction == "around")
+        {
+            rot.eulerAngles = new Vector3(0, 0, 180);
+        }
+        transform.rotation *= rot;
+    }
+
+    void move()
+    {
+        Vector3 motionVector = transform.TransformDirection(forward);
+        car.MovePosition(transform.position + motionVector * Time.deltaTime * speed);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "border")
+        {
+            turn("around");
+        }
+        else if (col.gameObject.tag == "intersection")
+        {
+            if (turned_collisions != 0)
+            {
+                turned_collisions -= 1;
+            }
+            else
+            {
+                collisions += 1;
+                if (collisions == 2)
+                {
+                    float should_turn = Random.value;
+                    if (should_turn <= .5f)
+                    {
+                        turn("right");
+                        turned_collisions = 1;
+                        collisions = 0;
+                    }
+                }
+                else if (collisions == 3)
+                {
+                    float should_turn = Random.value;
+                    if (should_turn <= .5f)
+                    {
+                        turn("left");
+                        turned_collisions = 2;
+                    }
+                    collisions = 0;
+                }
+            }
+        }
+    }
+}
